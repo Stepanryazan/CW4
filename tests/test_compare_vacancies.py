f20a8020dd49039f8634b2a5febd9069f222fa44
Stepanryazan <offish.sk@gmail.com>
2024-03-01 23:56:22 +0300

    first commit

--- tests/test_compare_vacancies.py
+++
@@ -1,9 +0,0 @@
-from src.HeadHunter import HeadHunter
-import pytest
-
-
-def test_get_vacancies():
-    assert ((HeadHunter.get_vacancies(HeadHunter(), "python", 1))['items'][0]['name']
-            == "Стажер-разработчик Python")
-    assert ((HeadHunter.get_vacancies(HeadHunter(), "python", "python"))['description']
-            == "can't convert page argument to positive integer")
